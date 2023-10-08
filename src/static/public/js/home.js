791d26841706c55ea197a25143852ce40e77be10
pedro-rbeiro <business.psycho@outlook.com>
2023-10-08 00:49:34 -0300

    Adicionado tela de carregamento

--- src/static/public/js/home.js
+++ src/static/public/js/home.js
@@ -1,3 +1,18 @@
+window.onload = () => {
+  const loadingScreen = document.getElementById('loading')
+
+  setTimeout(() => {
+    console.log("Delayed for 4 seconds.");
+    loadingScreen.classList.add('loaded')
+    console.log("added class");
+    setTimeout(() => {
+      console.log("Delayed for 5 seconds.");
+      document.getElementById('section-flex').removeChild(loadingScreen)
+    }, 3000);
+  }, 4000);
+
+
+}
 const hamburgerNav = document.getElementById('hamburger-nav')
 const hamburger = document.getElementById("hamburger-icon")
 hamburger.addEventListener('click', () => {
@@ -11,3 +26,39 @@ hamburger.addEventListener('click', () => {
   }
 
 })
+const colors = [
+  "#FFFFFF",
+  "#8F00FF",
+];
+
+const squares = document.querySelectorAll(".Preloader-square");
+
+const tl = new TimelineMax({ onComplete: Test });
+
+const anim = function () {
+  tl.staggerFromTo(squares, 0.8,
+    {
+      height: 0,
+    },
+    {
+      cycle: {
+        backgroundColor() {
+          return colors[Math.floor(Math.random() * colors.length)];
+        },
+        height() {
+          return (Math.random() * 40) + 10;
+        }
+      },
+      autoAlpha: 1,
+      ease: Expo.easeInOut,
+    },
+    0.4,
+  );
+  tl.staggerTo(squares, 0.7, { autoAlpha: 0, height: 0, ease: Expo.easeInOut }, 0.4, "-=0.5");
+  console.log(tl);
+};
+
+function Test() {
+  tl.invalidate();
+  anim();
+};
