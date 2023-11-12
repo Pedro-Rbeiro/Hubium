670a1827845016108ee55e904af9aa79fcdff7be
Magual27 <miguenrique27@gmail.com>
2023-11-12 11:17:59 -0300

    correção select das músicas

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
     cookie: {
       secure: false,
       maxAge: 360000,
-      expires: new Date(Date.now() + 360000),
+      expires: new Date(Date.now() + 720000),
       httpOnly: true,
     },
   })
